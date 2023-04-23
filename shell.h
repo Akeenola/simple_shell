566673f737924ae34f061fdd8df94714de53f519
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-04-23 13:40:10 +0100

    shell header file

--- shell.h
+++ shell.h
@@ -18,7 +18,7 @@ extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 
 /* handles build ins */
 int checker(char **cmd, char *buf);
-void prompt_user(void);
+void prompt(void);
 void handle_signal(int m);
 char **tokenizer(char *line);
 char *check_path(char **path, char *cmd);
