a8bb3f746130366b559e0bebace53b8b9d3c0362
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-04-24 18:45:34 +0100

    check function

--- check.c
+++ check.c
@@ -2,7 +2,7 @@
 /**
  *check- checks to see weather its a built in function
  *@cmd: tokenized user input
- *@buf: line drived fromgetline function
+ *@buf: line drived from getline function
  *Return: 1 if cmd excuted, 0 if cmd is not executed
  */
 int check(char **cmd, char *buf)
@@ -11,7 +11,7 @@ int check(char **cmd, char *buf)
 		return (1);
 	else if (**cmd == '/')
 	{
-		execution(cmd[0], cmd);
+		execute(cmd[0], cmd);
 		return (1);
 	}
 	return (0);
