3cc00ab52714e04e37892f88c3c94e37c7ec9091
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-04-25 21:38:39 +0100

    shell main function

--- shell.c
+++ shell.c
@@ -1,50 +1,87 @@
 #include "shell.h"
-/**
-* main - reads instructions with args, execute then print output loop
-* @argc: argument count
-* @argv: argument vector
-* @env: environment vector
-*
-* Return: 0
-*/
 
-int main(int argc, char **argv, char *env[])
+/**
+ * execute - executes the command
+ * @cmd: command to run
+ * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
+ */
+int execute(char **cmd)
 {
-	char *line = NULL, *pathcommand = NULL, *path = NULL;
-	size_t buf = 0;
-	ssize_t linesize = 0;
-	char **command = NULL, **paths = NULL;
-	(void)env, (void)argv;
-	if (argc < 1)
+
+	pid_t child_pid;
+	int status;
+
+	if (strncmp("exit", cmd[0], 4) == 0)
 		return (-1);
-	signal(SIGINT, handle_signal);
-	while (1)
+
+	child_pid = fork();
+
+	if (child_pid == -1)
 	{
-		_free(command);
-		_free(paths);
-		free(pathcommand);
-		prompt();
-		linesize = getline(&line, &buf, stdin);
-		if (linesize < 0)
-			break;
-		info.ln_count++;
-		if (line[linesize - 1] == '\n')
-			line[linesize - 1] = '\0';
-		command = tokenizer(line);
-		if (command == NULL || *command == NULL || **command == '\0')
-			continue;
-		if (check(command, line))
-			continue;
-		path = find_path();
-		paths = tokenizer(path);
-		pathcommand = check_path(paths, command[0]);
-		if (!pathcommand)
-			perror(argv[0]);
-		else
-			execute(pathcommand, command);
+		perror("Error");
+		return (1);
 	}
-	if (linesize < 0 && flags.interactive)
-		write(STDERR_FILENO, "\n", 1);
-	free(line);
+	else if (child_pid == 0)
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+	}
+	else
+		wait(&status);
+
+	return (0);
+}
+
+
+/**
+ * main - main simple shell
+ * @argc: number of arguments
+ * @argv: list of command line arguments
+ * Return: Always 0, -1 on error.
+ */
+
+int main(int argc, char **argv)
+{
+
+	int response;
+	char **tokens;
+	size_t bufsize = BUFSIZ;
+	int isPipe = 0;
+	char *buffer;
+
+	if (argc >= 2)
+	{
+		/*TODO: Handle cases where there is no argument, only the command*/
+		if (execve(argv[1], argv, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+		return (0);
+	}
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+	do {
+		if (isatty(fileno(stdin)))
+		{
+			isPipe = 1;
+			_puts("cisfun#: ");
+		}
+
+		getline(&buffer, &bufsize, stdin);
+		buffer[_strlen(buffer) - 1] = '\0';
+		tokens = stringToTokens(buffer);
+		response = execute(tokens);
+	} while (isPipe && response != -1);
+
 	return (0);
 }
