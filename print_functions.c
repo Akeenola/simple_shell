1530c117c3f1002d6d356a344c7532bb9508810c
Akeenola <68507542+Akeenola@users.noreply.github.com>
2023-04-26 02:59:50 +0100

    print functions done

--- print_functions.c
+++ print_functions.c
@@ -1,4 +1,18 @@
 #include "shell.h"
+#include<unistd.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * _puts - prints a string
@@ -16,14 +30,3 @@ while (str[i])
 }
 }
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
